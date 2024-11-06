7215e4760fb3fbf437059093bd5036ac66fba7e4
AndrBy1 <abyi@ucsc.edu>
2024-11-06 22:23:33 +0000

    Added button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 // todo
+// todo
+import "./style.css";
+// @deno-types="npm:@types/leaflet@^1.9.14"
+
+
+const message = "You clicked the button!";
+const Button1 = document.createElement("button");
+Button1.textContent = "Click";
+document.body.append(Button1);
