ad3159a69ab66f6953b1c03da9bd706f0f7945c4
AndrBy1 <abyi@ucsc.edu>
2024-11-06 22:24:08 +0000

    added event listener for the button

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,7 @@ const message = "You clicked the button!";
 const Button1 = document.createElement("button");
 Button1.textContent = "Click";
 document.body.append(Button1);
+
+Button1.addEventListener("click", () => {
+  alert(message);
+});
