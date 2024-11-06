fe38372819a323d9d9893bba66fc24b635accfad
AndrBy1 <abyi@ucsc.edu>
2024-11-06 22:27:28 +0000

    Added map to tile layer

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,15 @@ Button1.addEventListener("click", () => {
   alert(message);
 });
 
-
 const playerLocation = leaflet.latLng(36.98949379578401, -122.06277128548504);
 
 const map = leaflet.map("map", {
   center: playerLocation,
   zoom: 13,
 });
+
+leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
