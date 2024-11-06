3ed23cc361885dc7e9c14860236b42192cbe4b2e
AndrBy1 <abyi@ucsc.edu>
2024-11-06 22:35:29 +0000

    imported leaflet.css to fix map

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 // todo
 // todo
-import "./style.css";
-
 // @deno-types="npm:@types/leaflet@^1.9.14"
 import leaflet from "leaflet";
+
+import "leaflet/dist/leaflet.css";
+import "./style.css";
+
+
 
 const message = "You clicked the button!";
 const Button1 = document.createElement("button");
