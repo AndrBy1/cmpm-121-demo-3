dd3eb76ea84dc138230212fba138541a3a757bdb
AndrBy1 <abyi@ucsc.edu>
2024-11-08 22:42:16 +0000

    added more to board

--- src/board.ts
+++ src/board.ts
@@ -5,6 +5,13 @@ import leaflet from "leaflet";
 interface Cell {
   readonly i: number;
   readonly j: number;
+}
+
+interface board {
+  readonly map: leaflet.Map;
+  readonly knownCells: Map<string, Cell>;
+  readonly tileWidth: number;
+  readonly tileVisibilityRadius: number;
   generateCache(x: number, y: number): void;
   genRandom(min: number, max: number): number;
   popupButtonClick(
@@ -12,4 +19,9 @@ interface Cell {
     coinNum: number,
     content: HTMLDivElement,
   ): number;
+  setCell(x: number, y: number): Cell;
+  getCanonicalCell(cell: Cell): Cell;
+  getCellForPoint(point: leaflet.LatLng): Cell;
+  getCellBounds(cell: Cell): leaflet.latLngBounds;
+  getCellsAround(point: leaflet.latLng): Cell[];
 }
