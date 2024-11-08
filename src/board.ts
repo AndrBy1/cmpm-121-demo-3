46559af93b169f8924646122a7530e6703bdd62f
AndrBy1 <abyi@ucsc.edu>
2024-11-08 02:31:30 +0000

    added to cell interface

--- src/board.ts
+++ src/board.ts
@@ -5,4 +5,11 @@ import leaflet from "leaflet";
 interface Cell {
   readonly i: number;
   readonly j: number;
+  generateCache(x: number, y: number): void;
+  genRandom(min: number, max: number): number;
+  popupButtonClick(
+    collect: boolean,
+    coinNum: number,
+    content: HTMLDivElement,
+  ): number;
 }
