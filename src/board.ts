d01db85cb53b164cedc1d2e3eddd68a91166da98
AndrBy1 <abyi@ucsc.edu>
2024-11-09 07:32:31 +0000

    created getLatLngOfKnown and setNewCell

--- src/board.ts
+++ src/board.ts
@@ -14,12 +14,13 @@ interface board {
   knownCells: Cell[];
   //map: leaflet.Map;
   //getCanonicalCell(cell: Cell): Cell;
-
+  setNewCell(i: number, j: number): void;
   getCellForPoint(point: leaflet.LatLng): Cell;
   getCellBounds(cell: Cell): leaflet.LatLngBounds;
   getCellsNearPoint(point: leaflet.LatLng): Cell[];
   getLatLngOfCell(cell: Cell): leaflet.latLng; //convert cell to leaflet.latLng because leaflet doesn't recognize interface coordinates
   getPlayerLatLng(): leaflet.LatLng;
+  getLatLngOfKnown(index: number): leaflet.latLng;
 }
 
 export const accessBoard: board = {
@@ -39,7 +40,9 @@ export const accessBoard: board = {
     // ...
     return this.knownCells.get(key)!;
   },*/
-
+  setNewCell(i: number, j: number): void {
+    this.knownCells.push({ i: i, j: j });
+  },
   getCellForPoint(point: leaflet.LatLng): Cell {
     return this.knownCells[
       this.knownCells.indexOf({
@@ -63,8 +66,10 @@ export const accessBoard: board = {
   getLatLngOfCell(cell: Cell): leaflet.LatLng {
     return leaflet.latLng(cell.i * this.cellDegrees, cell.j * this.cellDegrees);
   },
-
   getPlayerLatLng(): leaflet.LatLng {
     return this.getLatLngOfCell(this.playerLocation);
+  },
+  getLatLngOfKnown(index: number): leaflet.latLng {
+    return this.getLatLngOfCell(this.knownCells[index]);
   },
 };
